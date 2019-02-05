675ab4d37db42fe2702c994cb97f57e0a51ffc11
gabewei <gweib5495@westada.org>
2019-02-05 12:38:20 -0700

    New updates 12:38 pm

--- S201AP4/src/ArrayTester.java
+++ S201AP4/src/ArrayTester.java
@@ -1,7 +1,17 @@
 import java.util.Arrays;
-
+/**
+ * 
+ * @author Gabe W
+ *
+ */
 public class ArrayTester {
 
+	/**
+	 * 4 AP Points (creates new array)
+	 * @param arr2D
+	 * @param c
+	 * @return
+	 */
 	public static int[] getColumn(int[][] arr2D, int c) {
 		int[] result = new int[arr2D.length];
 
@@ -11,6 +21,11 @@ public class ArrayTester {
 		return result;
 	}
 
+	/**
+	 * 5 AP Points (calls, applies, and returns)
+	 * @param square
+	 * @return
+	 */
 	public static boolean isLatin(int[][] square) {
 		if (containsDuplicates(square[0])) {
 			return false;
@@ -32,12 +47,40 @@ public class ArrayTester {
 		return true;
 
 	}
-
+/**
+ * 
+ * @param arr1
+ * @param arr2
+ * @return
+ */
 	private static boolean hasAllValues(int[] arr1, int[] arr2) {
-
-		return false;
-	}
-
+		    for (int a : arr1) {
+		        if (!contains(arr2, a)) {
+		            return false;
+		        }
+		    }
+		    return true;
+		    
+		}
+/**
+ * 
+ * @param list
+ * @param i
+ * @return
+ */
+	private static boolean contains(int[] list, int i) {
+        for (int a : list) {
+            if (a == i) {
+                return true;
+            }
+        }
+        return false;
+    }
+	/**
+	 * 
+	 * @param arr
+	 * @return
+	 */
 	private static boolean containsDuplicates(int[] arr) {
 		boolean duplicate = false;
 		for (int i = 0; i < arr.length; i++) {
@@ -49,14 +92,23 @@ public class ArrayTester {
 		}
 		return duplicate;
 	}
-
+/**
+ * 
+ * @param args
+ */
 	public static void main(String[] args) {
 		int[][] arr2D = {	{ 0, 1, 2},
 							{ 3, 4, 5},
 							{ 6, 7, 8},
 							{ 9, 5, 3} };
+		
+		int[][] Latin = {	{ 1, 2, 3},
+							{ 2, 3, 1},
+							{ 3, 1, 2} };
+						
 		int[] result = ArrayTester.getColumn(arr2D, 1);
-		System.out.println(Arrays.toString(result));
+		System.out.println(isLatin(arr2D));
+		System.out.println(isLatin(Latin));
 		}
 
 	}
